950ac61f506f649d9f1851f0f33f36a32c3241c7
Nam077 <nam077.work@gmail.com>
2025-08-09 17:56:19 +0700

    test: add mock for Example2Service in controller and service tests

--- src/modules/example/example.controller.spec.ts
+++ src/modules/example/example.controller.spec.ts
@@ -1,15 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { Example2Service } from '@/modules/example-2/example-2.service';
+
 import { ExampleController } from './example.controller';
 import { ExampleService } from './example.service';
 
 describe('ExampleController', () => {
     let controller: ExampleController;
 
+    const mockExample2Service = {
+        create: jest.fn(),
+        findAll: jest.fn(),
+        findOne: jest.fn(),
+        update: jest.fn(),
+        remove: jest.fn(),
+    };
+
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
             controllers: [ExampleController],
-            providers: [ExampleService],
+            providers: [
+                ExampleService,
+                {
+                    provide: Example2Service,
+                    useValue: mockExample2Service,
+                },
+            ],
         }).compile();
 
         controller = module.get<ExampleController>(ExampleController);
--- src/modules/example/example.service.spec.ts
+++ src/modules/example/example.service.spec.ts
@@ -1,13 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { Example2Service } from '@/modules/example-2/example-2.service';
+
 import { ExampleService } from './example.service';
 
 describe('ExampleService', () => {
     let service: ExampleService;
 
+    const mockExample2Service = {
+        create: jest.fn(),
+        findAll: jest.fn(),
+        findOne: jest.fn(),
+        update: jest.fn(),
+        remove: jest.fn(),
+    };
+
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [ExampleService],
+            providers: [
+                ExampleService,
+                {
+                    provide: Example2Service,
+                    useValue: mockExample2Service,
+                },
+            ],
         }).compile();
 
         service = module.get<ExampleService>(ExampleService);
