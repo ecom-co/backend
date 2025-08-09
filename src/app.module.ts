d05241c3c6c2955a8ec2a15bb2d74a6b4dca18b0
Nam077 <nam077.work@gmail.com>
2025-08-09 22:53:52 +0700

    feat: integrate TypeORM with new ORM package and update database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
+import { CORE_ENTITIES, TypeOrmModule } from '@ecom-co/orm';
 import { Module } from '@nestjs/common';
 import { ConfigModule as NestConfigModule } from '@nestjs/config';
 
 import { ConfigModule } from '@/modules/config/config.module';
+import { ConfigServiceApp } from '@/modules/config/config.service';
 import { ExampleModule } from '@/modules/example/example.module';
 
 import { AppController } from '@/app.controller';
@@ -10,7 +12,24 @@ import { AppService } from '@/app.service';
 import { Example2Module } from './modules/example-2/example-2.module';
 
 @Module({
-    imports: [NestConfigModule.forRoot(), ConfigModule, ExampleModule, Example2Module],
+    imports: [
+        NestConfigModule.forRoot(),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigServiceApp],
+            useFactory: (configService: ConfigServiceApp) => ({
+                type: 'postgres',
+                url: configService.databaseUrl,
+                synchronize: configService.isDevelopment,
+                logging: configService.isDevelopment,
+                entities: [...CORE_ENTITIES],
+                autoLoadEntities: true,
+            }),
+        }),
+        ConfigModule,
+        ExampleModule,
+        Example2Module,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
--- src/modules/example/example.module.ts
+++ src/modules/example/example.module.ts
@@ -1,3 +1,4 @@
+import { TypeOrmModule, User } from '@ecom-co/orm';
 import { Module } from '@nestjs/common';
 
 import { Example2Module } from '@/modules/example-2/example-2.module';
@@ -6,7 +7,7 @@ import { ExampleController } from './example.controller';
 import { ExampleService } from './example.service';
 
 @Module({
-    imports: [Example2Module], // using for dependency injection for ExampleService2 (Example2Service)
+    imports: [Example2Module, TypeOrmModule.forFeature([User])],
     controllers: [ExampleController],
     providers: [ExampleService],
 })
