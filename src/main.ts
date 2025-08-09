de91d9a3598dff4baec4828ac55e29c6fb10625f
Nam077 <nam077.work@gmail.com>
2025-08-09 16:14:52 +0700

    chore: init project

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app.module';
+
+/**
+ *
+ */
+const bootstrap = async () => {
+    const app = await NestFactory.create(AppModule);
+    await app.listen(process.env.PORT ?? 3000);
+};
+
+void (async (): Promise<void> => {
+    await bootstrap();
+})();
